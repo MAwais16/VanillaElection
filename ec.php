9503085b9e6c61cff5579802f7bf55024fe5f9e3
MAwais16 <muhammad.a.akhtar@student.fh-kiel.de>
2014-12-22 01:29:15 +0100

    initialized

---
+++ ec.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Fotaxis;
+
+
+//EC
+
+class EC{
+
+	public function EC() {
+        // treated as constructor in PHP 5.3.0-5.3.2
+        // treated as regular method as of PHP 5.3.3
+    }
+
+    
+
+
+?>
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Vanilla Election
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: For PSA-Kiel online Election
+ * Version: 0.1
+ * Author: Muhammad Awais Akhtar
+ * Author Email: awaisakhtar16@yahoo.com
+ * License:GPL2
+ */
+
+
+require_once("./ec.php");
+use \Fotaxis\EC;
+
+$election= new EC();
+
+
+
+?>
